903cb57d8f9cb7b2328b9fe259b101c714fe3269
Dawid Bialy <dbialy@tvn.pl>
2018-12-07 16:55:10 +0100

    Added solution to day 3, added descroiption to day 4

--- README.md
+++ README.md
@@ -7,8 +7,10 @@ I might use various languages to provide solutions... will see how it goes ;).
 |:---:|:------------------------------------------------------------------|:---------------------:|:------:|
 | 01  | [Chronal Calibration](/tasks/day01.md)                            | [GO](day01/day01.go)   |    ✔   |
 | 02  | [Inventory Management System](/tasks/day02.md)                    | [GO](day02/day02.go)   |    ✔   |
-| 03  | [No Matter How You Slice It](/tasks/day03.md)                     | [GO](day03/day03.go)   |    -   |
+| 03  | [No Matter How You Slice It](/tasks/day03.md)                     | [GO](day03/day03.go)   |    ✔   |
+| 04  | [Repose Record](/tasks/day04.md)                                  | [GO](day03/day04.go)   |    -   |
 
 [day1]: http://adventofcode.com/2018/day/1
 [day2]: http://adventofcode.com/2018/day/2
 [day3]: http://adventofcode.com/2018/day/3
+[day4]: http://adventofcode.com/2018/day/4
--- day03/day03.go
+++ day03/day03.go
@@ -1,9 +1,87 @@
 package main
 
-const (
-	InputFile = "input"
+import (
+	"fmt"
+	"io/ioutil"
+	"path"
+	"regexp"
+	"runtime"
+	"strconv"
+	"strings"
 )
 
-func main() {
+const (
+	InputFile  = "input"
+	MatrixSize = 1000
+)
 
+var (
+	fabricMatrix     = [][]int{}
+	duplicates   int = 0
+)
+
+type square struct {
+	ID        int
+	positionX int
+	positionY int
+	width     int
+	height    int
+}
+
+// Parsing coordinates
+// example: #6 @ 362,248: 24x10
+func NewSquare(s string) *square {
+	re := regexp.MustCompile(`#([0-9]+)\s@\s([0-9]+),([0-9]+):\s([0-9]+)x([0-9]+)`)
+	match := re.FindStringSubmatch(s)
+
+	id, _ := strconv.Atoi(match[1])
+	positionX, _ := strconv.Atoi(match[2])
+	positionY, _ := strconv.Atoi(match[3])
+	width, _ := strconv.Atoi(match[4])
+	height, _ := strconv.Atoi(match[5])
+
+	return &square{id, positionX, positionY, width, height}
+}
+
+func main() {
+	vector := []int{}
+
+	for j := 0; j < MatrixSize; j++ {
+		vector = append(vector, 0)
+	}
+
+	for i := 0; i < MatrixSize; i++ {
+		v := append(vector[:0:0], vector...)
+		fabricMatrix = append(fabricMatrix, v)
+	}
+
+	_, fileName, _, _ := runtime.Caller(0)
+	filePath := path.Join(path.Dir(fileName), InputFile)
+	inpBuff, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	rows := strings.Split(string(inpBuff), "\n")
+	for _, row := range rows {
+		appenSquare(NewSquare(row))
+	}
+
+	for i := 0; i < MatrixSize; i++ {
+		for j := 0; j < MatrixSize; j++ {
+			if fabricMatrix[i][j] > 1 {
+				duplicates++
+			}
+		}
+	}
+
+	println("Duplicates: ", duplicates)
+}
+
+func appenSquare(s *square) {
+	for i := s.positionY; i < s.positionY+s.height; i++ {
+		for j := s.positionX; j < s.positionX+s.width; j++ {
+			fabricMatrix[i][j]++
+		}
+	}
 }
